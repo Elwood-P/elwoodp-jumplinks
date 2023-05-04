b8727ee1ee95d43e90adc2dd1e9b2cd06dc97b76
Elwood-P <info@pjlittlewood.co.uk>
2023-05-04 13:18:28 +0100

    Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ['./index.html'],
   theme: {
     extend: {
+      backgroundImage: {
+        hero: "linear-gradient(201.93deg, rgba(16, 24, 39, 0) 14.35%, rgba(16, 24, 39, 0.9) 54.41%), url('https://kimba-imagecdn.imgix.net/elwoodp-jumplinks/hero-bg.jpg?q=80&auto=format'), linear-gradient(0deg, rgba(16, 24, 39, 1), rgba(16, 24, 39, 1))",
+      },
+      borderRadius: {
+        DEFAULT: '12px',
+      },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
